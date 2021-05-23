ef0b678265ae90a69a041cb1d6cb521e3f67b875
jakegillespie6 <64338143+jakegillespie6@users.noreply.github.com>
2021-05-23 14:51:48 -0700

    Add files via upload

---
+++ circle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//This program prompts user to enter circle raduis
+//Then the circle area is displayed
+
+int main()
+{
+	double radius;
+	cout << "Enter radius\n";
+	//Add code to read user input and store in variable radius
+	cout << "The area of a circle with radius \n";
+	//Calculate the area of the circle
+	cout<<3.14 * radius * radius;
+	return 0;
+}
